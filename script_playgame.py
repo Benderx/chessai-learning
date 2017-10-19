0ab1e3590fb204381560d75c77204b60c46e7ea0
Matthew <mze5111@gmail.com>
2017-10-19 17:45:10 -0400

    Basic game tester

--- script_playgame.py
+++ script_playgame.py
@@ -2,5 +2,22 @@ import Engine
 import Player
 
 winner = None
+engine = Engine.Engine()
+
+PLAYER_ONE = Player.AiRand(1,engine)
+PLAYER_TWO = Player.AiRand(1,engine)
+
+players = [PLAYER_ONE,PLAYER_TWO]
+
+turn = 0
 
 while not winner:
+	possible_moves = engine.get_legal_moves(players[turn].get_color())
+	winner = engine.is_terminal(players[turn].get_color(),possible_moves)
+	print("\n\nBoardstate:")
+	engine.print_board()
+	print("Current players turn:", players[turn])
+	move = players[turn].get_move(possible_moves)
+	print("Move:", move)
+	engine.update_board(move)
+	turn = 1-turn
