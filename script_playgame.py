fa6b3233269f97c0328b926a2d154246d92e189a
Matthew <mze5111@gmail.com>
2017-10-20 14:10:27 -0400

    updated print order

--- script_playgame.py
+++ script_playgame.py
@@ -14,17 +14,17 @@ players = [PLAYER_ONE,PLAYER_TWO]
 turn = 0
 
 while True:
-	possible_moves = engine.get_legal_moves(players[turn].get_color())
-	winner = engine.is_terminal(players[turn].get_color(), possible_moves)
-	if winner:
-		print("Results are in:",winner)
-		break
-	print("\n\nBoardstate:")
-	engine.print_board()
-	print("Current players turn:", players[turn].get_color())
-	print("All moves:\n",possible_moves)
-	move = players[turn].get_move(possible_moves)
-	print("Move:", move)
-	engine.update_board(move)
-	turn = 1-turn
-	# time.sleep(1)
+    possible_moves = engine.get_legal_moves(players[turn].get_color())
+    winner = engine.is_terminal(players[turn].get_color(), possible_moves)
+    print("\n\nBoardstate:")
+    engine.print_board()
+    print("Current players turn:", players[turn].get_color())
+    print("All moves:\n",possible_moves)
+    if winner:
+        print("Results are in:",winner)
+        break
+    move = players[turn].get_move(possible_moves)
+    print("Move:", move)
+    engine.update_board(move)
+    turn = 1-turn
+    # time.sleep(1)
