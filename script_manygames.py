0f1def3367e6bcd7e0bdda296615a453b9e5a4e1
Matthew <mze5111@gmail.com>
2017-10-20 17:39:56 -0400

    For running many games

---
+++ script_manygames.py
@@ -0,0 +1,52 @@
+import Engine
+import Player
+import time
+
+play_by_play = False
+ww = 0
+bw = 0
+d = 0
+games = 1000
+while games > 0:
+    winner = None
+    engine = Engine.Engine()
+    engine.init_board()
+
+    PLAYER_ONE = Player.AiRand(1,engine)
+    PLAYER_TWO = Player.AiRand(-1,engine)
+
+    players = [PLAYER_ONE,PLAYER_TWO]
+
+    turn = 0
+
+    while True:
+        possible_moves = engine.get_legal_moves(players[turn].get_color())
+        winner = engine.is_terminal(players[turn].get_color(), possible_moves)
+        # print("\n\nBoardstate:")
+        # engine.print_board()
+        # print("Current players turn:", players[turn].get_color())
+        # print("All moves:\n",possible_moves)
+        if winner != None:
+            if winner == 1:
+                print("Results are in: White wins")
+                ww+=1
+            elif winner == -1:
+                print("Results are in: Black wins")
+                bw+=1
+            else:
+                print("Results are in: Draw")
+                d +=1
+            break
+        move = players[turn].get_move(possible_moves)
+        # print("Move:", move)
+        engine.update_board(move)
+        turn = 1-turn
+        # print(engine.get_game_length())
+        if play_by_play:
+            input("")
+    # print(games)
+    games-=1
+print("White won:",ww)
+print("Black won:",bw)
+print("There were draws: ",d)
+    # time.sleep(1)
