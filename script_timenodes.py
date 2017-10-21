37f491ca851def5a49741e40e4a82e60530336d9
Andrew Duerig <aqd5396@psu.edu>
2017-10-21 12:34:05 -0400

    more timing

--- script_timenodes.py
+++ script_timenodes.py
@@ -2,42 +2,92 @@ import Engine
 import Player
 import time
 
-engine = Engine.Engine()
-engine.init_board()
+# def play_game():
+#     engine = Engine.Engine()
+#     engine.init_board()
 
-PLAYER_ONE = Player.AiRand(1, engine)
-PLAYER_TWO = Player.AiRand(-1, engine)
+#     PLAYER_ONE = Player.AiRand(1, engine)
+#     PLAYER_TWO = Player.AiRand(-1, engine)
 
-players = [PLAYER_ONE,PLAYER_TWO]
+#     players = [PLAYER_ONE,PLAYER_TWO]
 
-turn = 0
+#     turn = 0
 
-while True:
-    if play_by_play:
-        input("")
+#     i_time = time.clock()
+#     while True:
+#         possible_moves = engine.get_legal_moves(players[turn].get_color())
+#         winner = engine.is_terminal(players[turn].get_color(), possible_moves)
+#         if winner != None:
+#             break
 
-    possible_moves = engine.get_legal_moves(players[turn].get_color())
-    winner = engine.is_terminal(players[turn].get_color(), possible_moves)
-    print('It is move:', engine.get_game_length())
-    print("Current players turn:", players[turn].get_color())
-    # print("All moves:\n",possible_moves)
-    if winner != None:
-        if winner == 1:
-            print("Results are in: White wins")
-        elif winner == -1:
-            print("Results are in: Black wins")
-        else:
-            print("Results are in: Draw")
-        break
-    move = players[turn].get_move(possible_moves)
-    print("Move:", move)
-    if move[0] != None:
-        print('Piece moved:', engine.get_board()[move[0][1]][move[0][0]].get_piece())
-    else:
-        print('Piece moved:', 'castle or something')
-    engine.perform_move(move)
-    print("Boardstate:")
-    engine.print_board()
-    print('\n')
+#         move = players[turn].get_move(possible_moves)
+#         engine.perform_move(move)
 
-    turn = 1-turn
+#         turn = 1-turn
+#     e_time = time.clock()
+#     return (e_time - i_time) / float(engine.get_game_length())
+
+
+# total = 0
+# total_i = 0
+# for i in range(100):
+#     total += play_game()
+#     total_i += 1
+# print('Nodes per second over {0} games: {1}'.format(total_i, 1.0/(total/total_i)))
+
+
+
+def get_time():
+    engine = Engine.Engine()
+    engine.init_board()
+
+    PLAYER_ONE = Player.AiRand(1, engine)
+    PLAYER_TWO = Player.AiRand(-1, engine)
+
+    players = [PLAYER_ONE,PLAYER_TWO]
+
+    turn = 0
+    num = 10000
+
+    while True:
+
+        i_time = time.clock()
+        for j in range(num):
+            possible_moves = engine.get_legal_moves(players[turn].get_color())
+        e_time = time.clock()
+
+        calc1 = (e_time-i_time)/float(num)
+        print('time taken on get_legal_moves over {0} iterations on turn {1}: {2:.{3}e}'.format(num, engine.get_game_length(), calc1, 4))
+        
+
+
+        i_time = time.clock()
+        for j in range(num):
+            winner = engine.is_terminal(players[turn].get_color(), possible_moves)
+        e_time = time.clock()
+
+        calc2 = (e_time-i_time)/float(num)
+        print('time taken on is_terminal over {0} iterations on turn {1}: {2:.{3}e}'.format(num, engine.get_game_length(), calc2, 4))
+
+
+
+        if winner != None:
+            break
+
+
+        i_time = time.clock()
+        for j in range(num):
+            move = players[turn].get_move(possible_moves)
+        e_time = time.clock()
+
+        calc3 = (e_time-i_time)/float(num)
+        print('time taken on get_move over {0} iterations on turn {1}: {2:.{3}e}'.format(num, engine.get_game_length(), calc3, 4))
+
+        print()
+
+        engine.perform_move(move)
+
+        turn = 1-turn
+
+
+t = get_time()
