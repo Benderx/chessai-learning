f33378bd31e751bee829cde55eaa46dde29b6832
Matthew <mze5111@gmail.com>
2017-10-24 17:03:23 -0400

    Framework for encoding and decoding moves

--- Bitboard.py
+++ Bitboard.py
@@ -129,6 +129,15 @@ class BitboardEngine():
         return(all_pieces)
 
 
+    def encode_move(self,start,end,flags):
+        encode_start = np.uint8(start)
+        encode_end = np.uint16(end) << np.uint16(6)
+        encode_flags = np.uint32() << 12
+
+    def decode_move(self,move):
+        pass
+
+
     def reverse_8_bit(self, row):
         num = np.uint8(row)
         reverse_num = np.uint8(row)
