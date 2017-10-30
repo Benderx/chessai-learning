b479808a8bdfac1f7211dec550480feaac56282c
acninetyfive <acninetyfive@gmail.com>
2017-10-29 21:50:38 -0400

    mproved minimax with alpha/beta cutoff

--- Player.py
+++ Player.py
@@ -36,119 +36,175 @@ class AiRand(Player):
 class AiMinimax(Player):
     def __init__(self, color, engine):
         super().__init__(color, engine)
-        print (self.color)
 
     def get_move(self, moves):
         #return(random.choice(moves))
         m_arr = moves
         moves_arr = []
-        #print("m_arr: " + str(m_arr))
         for x in m_arr:
-            #print("testing move " + str(x))
             self.engine.push_move(x)
-            #print(self.move_stack)
-            #print('')
-            #self.engine.print_board()
-            value = self.minimax(2, self.color * -1, math.inf * -1, math.inf)
-            if value != 0:
-                print ("move " + str(x) + " eval " + str(value))
-            #print(self.move_stack)
+            value = self.minimax(1, math.inf * -1, math.inf, self.color * -1)
+            #if value != 0:
+                #print ("move " + str(x) + " eval " + str(value))
             self.engine.pop_move()
-            moves_arr.append((self.color * value, x))
-        #print ("moves_arr: " + str(moves_arr))
+            moves_arr.append((value, x)) #normalize so both colors look for largest val
+        #print("moves: ")
+        #for x in moves_arr:
+        #    print(x)
         best_move = moves_arr[0]
         b_arr = [best_move]
-        for x in moves_arr:
-            #print("x = "+str(x))
-            #print("best_move = " + str(best_move))
-            if x[0] > best_move[0]:
-                #print("x is better")
-                b_arr = [x]
-                best_move = x
-            elif x[0] == best_move[0]:
-                b_arr.append(x)
-            #print("best_move = " + str(best_move))
-        #print(moves_arr)
-        #print(b_arr)
-        
+        if self.color == 1: #white, look for most positive value
+            for x in moves_arr:
+                if x[0] > best_move[0]:
+                    b_arr = [x]
+                    best_move = x
+                elif x[0] == best_move[0]:
+                    b_arr.append(x)
+        else: #black, look for most negative value
+            for x in moves_arr:
+                if x[0] < best_move[0]:
+                    b_arr = [x]
+                    best_move = x
+                elif x[0] == best_move[0]:
+                    b_arr.append(x)
+            
         best_move = random.choice(b_arr)
         if len(b_arr) < 10:
             print ("best moves " + str(b_arr))
+        print (best_move[0])
         return best_move[1]
 
     def get_type(self): #could just inherit with self.type
         return('Minimax AI')
 
-    def evaluate(self, color): #Returns a score for the current board state
+    def evaluate(self, color, depth): #Returns a score for the current board state
         score = 0.0
-        w = self.engine.is_terminal(color,self.engine.get_legal_moves(color)) #check if current color lost 
+
+        own_moves = self.engine.get_legal_moves(color)
+        enemy_moves = self.engine.get_legal_moves(color * -1)
+
+        own_pieces = []
+        enemy_pieces = []
+
+        w = self.engine.is_terminal(color, own_moves) #check if current color lost 
         if w != None:
             if w == 0:
                 score = 0
                 return score
             else:
-                score = math.inf * w #return math.inf on white win, -inf on black win
+                score = (1000 + depth)  * w  #return math.inf on white win, -inf on black win
                 return score
-        if self.engine.in_check(-1*color):
-            score += 0.51
-            print("CHECK")
-            self.engine.print_board()
+        check = self.engine.in_check(color)
+        if check:
+            if len(own_moves) == 0:
+                return (1000 + depth) * -1 * color
+            else:
+                score += 0.51 * color * -1
+        if len(own_moves) == 0: #stalemate
+            print("saw stalemate")
+            return 0.0
+
+
 
         board = self.engine.get_board()
-        for row in board:
-            for square in row:
+        for row in range(8):
+            for col in range(8):
+                square = board[col][row]
                 if square != None:
                     if square.get_piece() == "Pawn":
+                        if square.get_color() == color:
+                            own_pieces.append(("Pawn", (row,col)))
+                        else:
+                            enemy_pieces.append(("Pawn", (row,col)))
                         score += 1 * square.get_color()
                     elif square.get_piece() == "Night":
+                        if square.get_color() == color:
+                            own_pieces.append(("Night", (row,col)))
+                        else:
+                            enemy_pieces.append(("Night", (row,col)))
                         score += 3 * square.get_color()
                     elif square.get_piece() == "Bishop":
+                        if square.get_color() == color:
+                            own_pieces.append(("Bishop", (row,col)))
+                        else:
+                            enemy_pieces.append(("Bishop", (row,col)))
                         score += 3 * square.get_color()
                     elif square.get_piece() == "Rook":
+                        if square.get_color() == color:
+                            own_pieces.append(("Rook", (row,col)))
+                        else:
+                            enemy_pieces.append(("Rook", (row,col)))
                         score += 4 * square.get_color()
                     elif square.get_piece() == "Queen":
+                        if square.get_color() == color:
+                            own_pieces.append(("Queen", (row,col)))
+                        else:
+                            enemy_pieces.append(("Queen", (row,col)))
                         score += 9 * square.get_color()
+                    elif square.get_piece() == "King":
+                        if square.get_color() == color:
+                            #own_pieces.append(("King", (row,col)))
+                            own_king_pos = (row,col)
+                        else:
+                            #enemy_pieces.append(("King", (row,col)))
+                            enemy_king_pos = (row,col)
+        if len(own_pieces) <= 1 and len(own_pieces) != 0:
+            score += -0.01 * self.get_distance(own_king_pos, enemy_king_pos) * color * -1
+            if enemy_king_pos[0] == 0 or enemy_king_pos[1] == 0:
+                score += .05 * color
+            #when few pieces remain, incentivize king to get involved
+
+        if color == 1:
+            score += .1 * (len(own_moves) - len(enemy_moves))
+        else: #always calculate white moves - black moves
+            score += .1 * (len(enemy_moves) - len(own_moves))
+        #print (own_pieces)
         return score
 
-    def minimax(self, depth, color, minimum, imum):
-        val = self.evaluate(color)
+    def minimax(self, depth, alpha, beta, color):
+        val = self.evaluate(color, depth)
+        #print (val)
 
-        if depth == 0 or val == math.inf or val == (-1 * math.inf):            
+        if depth == 0 or val > 900 or val < -900:            
             return val
         
-        if color == self.color:
-            a = self.engine.get_legal_moves(color)
-            v = minimum
-            for x in a:
-                #print ("testing move ", x)
+        move_list = self.engine.get_legal_moves(color)
+
+        if color == 1: #white
+            v = -1 * math.inf
+            for x in move_list:
                 self.engine.push_move(x)
-                v1 = self.minimax(depth - 1, -1 * self.color, v, maximum)
+                v = max(v, self.minimax(depth - 1, alpha, beta, -1))
                 self.engine.pop_move()  
-                if v1 > v:
-                    v = v1
-                if v > maximum:
-                    return maximum
+                if beta <= alpha:
+                    print("beta cutoff")
+                    break 
             return v
             
-        else: #not computer's turn
-            b = self.engine.get_legal_moves(color)
-            w = maximum 
-            for x in b:
+        else: #black
+            v = math.inf 
+            for x in move_list:
                 self.engine.push_move(x)
-                w1 = self.minimax(depth - 1, self.color, minimum, w)
+                v = min(v, self.minimax(depth - 1, alpha, beta, 1))
                 self.engine.pop_move()
-                if w1 < w:
-                    w = w1
-                if w < minimum:
-                    return minimum
-            return w
+                beta = min(beta, v)
+                if beta <= alpha:
+                    print("alpha cutoff")
+                    break
+            return v
+
+    def get_distance(self, a, b): #takes in 2 tuples, (x1,y1), (x2,y2)
+        return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1]) **2)
+
 
 class Human(Player):
     def __init__(self, color, engine, renderer):
         super().__init__(color, engine)
-        self.renderer = renderer
+        #self.renderer = renderer
 
     def get_move(self, moves):
+        #move = input("move? Format ((x,y),(x,y), None, 'normal')")
+        #return move
         raise Exception('get_move for player is not implemented')
         # win = self.renderer.get_window()
         # maxWidth = self.renderer.get_width()
--- script_playgame.py
+++ script_playgame.py
@@ -3,72 +3,112 @@ import Player
 import time
 import Piece
 
-board = [[None for x in range(8)] for y in range(8)]
 
-
-
-
-play_by_play = False
-
-winner = None
-engine = Engine.Engine()
-
+'''
 
 engine.white_king = Piece.King(1)
 engine.white_king_pos = (4, 7)
+
+p1 = Piece.Pawn(1)
+p2 = Piece.Pawn(1)
+p3 = Piece.Pawn(-1)
+
 white_queen = Piece.Queen(1)
 board[7][4] = engine.white_king
-board[7][3] = white_queen
+board[6][4] = p1
+board[6][5] = p2
+#board[7][3] = white_queen
 
 engine.black_king = Piece.King(-1)
 engine.black_king_pos = (4, 0)
 black_queen = Piece.Queen(-1)
+board[4][1] = p3
 board[0][4] = engine.black_king
-board[0][3] = black_queen
+board[4][0] = black_queen
+'''
+game_list = []
 
-engine.init_board(board)
+for x in range(1):
+    board = [[None for x in range(8)] for y in range(8)]
 
-PLAYER_ONE = Player.AiRand(1,engine)
-PLAYER_TWO = Player.AiMinimax(-1,engine)
+    play_by_play = False
 
-players = [PLAYER_ONE,PLAYER_TWO]
+    winner = None
+    engine = Engine.Engine()
 
-turn = 0
+    engine.white_king = Piece.King(1)
+    engine.white_king_pos = (4, 7)
 
-print("Inital Boardstate:")
-engine.print_board()
+    p1 = Piece.Pawn(1)
+    p2 = Piece.Pawn(1)
+    p3 = Piece.Pawn(-1)
 
-while True:
-    if play_by_play:
-        input("")
+    white_queen = Piece.Queen(1)
+    board[7][4] = engine.white_king
+    #board[6][4] = p1
+    #board[6][5] = p2
+    #board[7][3] = white_queen
 
-    possible_moves = engine.get_legal_moves(players[turn].get_color())
-    winner = engine.is_terminal(players[turn].get_color(), possible_moves)
-    print("\n\nBoardstate:")
+    engine.black_king = Piece.King(-1)
+    engine.black_king_pos = (4, 0)
+    black_queen = Piece.Queen(-1)
+    black_rook1 = Piece.Rook(-1)
+    board[1][4] = p3
+    board[0][4] = engine.black_king
+    #board[4][0] = black_queen
+    #board[0][7] = black_rook1
+
+    engine.init_board()
+
+    PLAYER_ONE = Player.AiMinimax(1,engine)
+    PLAYER_TWO = Player.AiRand(-1,engine)
+
+    #print(PLAYER_TWO.evaluate(-1, 2))
+
+    players = [PLAYER_ONE,PLAYER_TWO]
+
+    turn = 0
+
+    print("Inital Boardstate:")
     engine.print_board()
-    print("Current players turn:", players[turn].get_color())
-    # print("All moves:\n",possible_moves)
-    if winner != None:
-        if winner == 1:
-            print("Results are in: White wins")
-        elif winner == -1:
-            print("Results are in: Black wins")
+
+    while True:
+        if play_by_play:
+            input("")
+
+        possible_moves = engine.get_legal_moves(players[turn].get_color())
+        winner = engine.is_terminal(players[turn].get_color(), possible_moves)
+        print("\n\nBoardstate:")
+        engine.print_board()
+        print("Current players turn:", players[turn].get_color())
+        # print("All moves:\n",possible_moves)
+        if winner != None:
+            if winner == 1:
+                print("Results are in: White wins")
+                game_list.append((1,engine.get_game_length()))
+            elif winner == -1:
+                print("Results are in: Black wins")
+                game_list.append((-1,engine.get_game_length()))
+            else:
+                print("Results are in: Draw")
+                game_list.append((0,engine.get_game_length()))
+            break
+        move = players[turn].get_move(possible_moves)
+        print("Move:", move)
+        if move[0] != None:
+            print('Piece moved:', engine.get_board()[move[0][1]][move[0][0]].get_piece())
         else:
-            print("Results are in: Draw")
-        break
-    move = players[turn].get_move(possible_moves)
-    print("Move:", move)
-    if move[0] != None:
-        print('Piece moved:', engine.get_board()[move[0][1]][move[0][0]].get_piece())
-    else:
-        print('Piece moved:', 'castle or something')
-    engine.perform_move(move)
-    print("Boardstate:")
-    engine.print_board()
-    print('\n')
+            print('Piece moved:', 'castle or something')
+        engine.perform_move(move)
+        print("Boardstate:")
+        engine.print_board()
+        print('\n')
 
-    turn = 1-turn
-    print(engine.get_game_length())
-    if play_by_play:
-        input("")
+        turn = 1-turn
+        print(engine.get_game_length())
+        if play_by_play:
+            input("")
     # time.sleep(1)
+
+f = open("tests.txt", 'w')
+f.write(str(game_list))
